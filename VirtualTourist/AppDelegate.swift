18a0bd0a7004698f99a6d9c8ef51dbeb53126c46
Jae-Seung Lee <jaeseung@gmail.com>
2017-11-16 16:09:52 -0500

    Added checkIfFirstLaunch() and application(:willFinishLaunchingWithOptions:) to check the first launch.

--- VirtualTourist/AppDelegate.swift
+++ VirtualTourist/AppDelegate.swift
@@ -18,6 +18,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         return true
     }
+    
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
+        checkIfFirstLaunch()
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
@@ -41,6 +46,15 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
+}
 
+extension AppDelegate {
+    func checkIfFirstLaunch() {
+        guard let _ = UserDefaults.standard.object(forKey: "HasLaunchedBefore") as? Bool else {
+            UserDefaults.standard.set(true, forKey: "HasLaunchedBefore")
+            UserDefaults.standard.synchronize()
+            return
+        }
+    }
 }
 
