d0b2a1350c7e96a527902755766780cb355ec35e
Jae-Seung Lee <jaeseung@gmail.com>
2017-11-16 15:59:21 -0500

    Added addPin(:) to add a pin through UILongPressGestureRecognizer

--- VirtualTourist/VTMapViewController.swift
+++ VirtualTourist/VTMapViewController.swift
@@ -11,6 +11,8 @@ import MapKit
 
 class VTMapViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -33,6 +35,17 @@ class VTMapViewController: UIViewController {
     }
     */
 
+    @IBAction func addPin(_ sender: UILongPressGestureRecognizer) {
+        let location = sender.location(in: mapView)
+        let coordinate = mapView.convert(location,toCoordinateFrom: mapView)
+        
+        // Add annotation:
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = coordinate
+        mapView.addAnnotation(annotation)
+        
+        print("\(location), \(coordinate)")
+    }
 }
 
 // MARK: - MKMapViewDelegate
