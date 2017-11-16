b495140aada45c1d4e5c74cf4c32d65e93249b42
Jae-Seung Lee <jaeseung@gmail.com>
2017-11-16 14:55:49 -0500

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Jae-Seung Lee on 11/16/17.
+//  Copyright © 2017 Jae-Seung Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
