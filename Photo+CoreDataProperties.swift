534b4f912e0982abf5f5c203260cc41fcdc268ea
Jae-Seung Lee <jaeseung@gmail.com>
2017-11-24 13:54:49 -0500

    Added VTModel and subclasses Pin and Photo

---
+++ Photo+CoreDataProperties.swift
@@ -0,0 +1,23 @@
+//
+//  Photo+CoreDataProperties.swift
+//  VirtualTourist
+//
+//  Created by Jae Seung Lee on 11/24/17.
+//  Copyright © 2017 Jae-Seung Lee. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension Photo {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Photo> {
+        return NSFetchRequest<Photo>(entityName: "Photo")
+    }
+
+    @NSManaged public var imageData: NSData?
+    @NSManaged public var pin: Pin?
+
+}
---
+++ Pin+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Pin+CoreDataClass.swift
+//  VirtualTourist
+//
+//  Created by Jae Seung Lee on 11/24/17.
+//  Copyright © 2017 Jae-Seung Lee. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+public class Pin: NSManagedObject {
+
+}
